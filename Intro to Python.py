baa131d5adc91e2309bed181fa6498d5f4de7eb4
mossimo5 <36167029+mossimo5@users.noreply.github.com>
2018-02-16 07:27:08 -0800

    Add files via upload

---
+++ Intro to Python.py
@@ -0,0 +1,49 @@
+# Jack G. Bingham --- Intro to Python --- 02/16/18
+
+
+print("How are you?")
+
+print(5)
+
+print(2+7)
+
+print(-4+11)
+
+print(3+2*2)
+
+print(3+2*2)
+
+print( (3+2) * 2)
+
+print(3.5+2.1)
+
+print(4.0+1.5)
+
+print(-1.1+1.1)
+
+print(6.5/2)
+
+print(14.5)
+
+print("Hello")
+
+print("He" + "llo")
+
+print("He" + "llo" * 5)
+
+print("Hello" * (2+2))
+
+print("Ha" *2 + "Ho" * 2)
+
+print("Hello" * (2 + 2))
+
+print("**\n**")
+
+print((("*" * 3) + "\n") * 3)
+
+print("****\n" * 4)
+
+name = input("What is your name? ")
+
+print ("Hello " + str(name) + "!!!")
+
